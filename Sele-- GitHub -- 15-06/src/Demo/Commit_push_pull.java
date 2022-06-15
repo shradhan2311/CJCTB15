ef2be1b9d4fc5f253e22050eba71ca913a57bf07
admin <admin@SHREE>
2022-06-15 18:00:16 +0530

    By shree

--- Sele-- GitHub -- 15-06/src/Demo/Commit_push_pull.java
+++ Sele-- GitHub -- 15-06/src/Demo/Commit_push_pull.java
@@ -1,7 +1,12 @@
 package Demo;
 
-public class Commit_push_pull {
-
+public class Commit_push_pull 
+{
+	
+   public void m1()
+  {
+	   System.out.println("hello gits by m1 Method ");
+  }
 	public static void main(String[] args) 
 	{
     System.out.println("hello Gits Demo");
